4db23af1a03cf6be901db2d8c0ff14a1338bd2a1
Omkar Khatavkar <okhatavkar007@gmail.com>
2019-08-03 21:19:55 +0530

    making single config file (#11)

--- plugins/jira_workflow.py
+++ plugins/jira_workflow.py
@@ -17,10 +17,10 @@ def start_issue_workflow(github_issues, jira, assignee=None):
             print("Not a Valid Result! Found more than 1 tasks for"
                   " github {}".format(issue_text))
         elif len(tasks) == 1:  # Issue Already Exist, Check Assignee and Status
-            echo_error("Updating Issue task for .....{}".format(issue.data['url']))
+            echo_error("Updating Issue task for .....{}".format(issue.data['html_url']))
             update_existing_issue_in_current_sprint(tasks[0], issue, jira=jira)
         elif len(tasks) == 0:  # Create new issue in backlog.
-            echo_error("Creating Issue task for.....{}".format(issue.data['url']))
+            echo_error("Creating Issue task for.....{}".format(issue.data['html_url']))
             create_new_issue_in_backlog(
                 issue,
                 jira=jira,
@@ -40,10 +40,10 @@ def start_create_pull_requests_workflow(github_issues, jira, assignee=None):
             print("Not a Valid Result! Found more than 1 tasks for"
                   " github {}".format(pr_text))
         elif len(tasks) == 1:  # Issue Already Exist, Check Assignee and Status
-            echo_error("Updating PR task for .....{}".format(issue.data['url']))
+            echo_error("Updating PR task for .....{}".format(issue.data['html_url']))
             update_existing_issue_in_current_sprint(tasks[0], issue, jira=jira)
         elif len(tasks) == 0:  # Create new issue in backlog.
-            echo_error("Creating PR task for.....{}".format(issue.data['url']))
+            echo_error("Creating PR task for.....{}".format(issue.data['html_url']))
             create_pull_request_in_current_sprint(
                 issue,
                 jira=jira,
@@ -63,10 +63,10 @@ def start_review_pull_requests_workflow(github_issues, jira, assignee=None):
             print("Not a Valid Result! Found more than 1 tasks for"
                   " github {}".format(pr_review_text))
         elif len(tasks) == 1:  # Issue Already Exist, Check Assignee and Status
-            echo_error("Updating PR review task for .....{}".format(issue.data['url']))
+            echo_error("Updating PR review task for .....{}".format(issue.data['html_url']))
             update_existing_issue_in_current_sprint(tasks[0], issue, jira=jira)
         elif len(tasks) == 0:  # Create new issue in backlog.
-            echo_error("Creating PR review task for .....{}".format(issue.data['url']))
+            echo_error("Creating PR review task for .....{}".format(issue.data['html_url']))
             create_pull_request_in_current_sprint(
                 issue,
                 jira=jira,
